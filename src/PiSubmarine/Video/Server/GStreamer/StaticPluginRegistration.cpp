9e1923891aaa8861a125de4a522081490f81a844
Dmitriy Monakhov <dimamon6032403@gmail.com>
2026-06-12 23:57:55 +0200

    Windows-specific changes

--- src/PiSubmarine/Video/Server/GStreamer/StaticPluginRegistration.cpp
+++ src/PiSubmarine/Video/Server/GStreamer/StaticPluginRegistration.cpp
@@ -11,10 +11,14 @@ extern "C"
 GST_PLUGIN_STATIC_DECLARE(coreelements);
 GST_PLUGIN_STATIC_DECLARE(autodetect);
 GST_PLUGIN_STATIC_DECLARE(videoconvertscale);
-GST_PLUGIN_STATIC_DECLARE(video4linux2);
 GST_PLUGIN_STATIC_DECLARE(openh264);
 GST_PLUGIN_STATIC_DECLARE(rtp);
 GST_PLUGIN_STATIC_DECLARE(udp);
+#if defined(_WIN32)
+GST_PLUGIN_STATIC_DECLARE(mediafoundation);
+#else
+GST_PLUGIN_STATIC_DECLARE(video4linux2);
+#endif
 }
 #endif
 
@@ -36,14 +40,19 @@ namespace PiSubmarine::Video::Server::GStreamer
             GST_PLUGIN_STATIC_REGISTER(autodetect);
             SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'videoconvertscale'");
             GST_PLUGIN_STATIC_REGISTER(videoconvertscale);
-            SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'video4linux2'");
-            GST_PLUGIN_STATIC_REGISTER(video4linux2);
             SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'openh264'");
             GST_PLUGIN_STATIC_REGISTER(openh264);
             SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'rtp'");
             GST_PLUGIN_STATIC_REGISTER(rtp);
             SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'udp'");
             GST_PLUGIN_STATIC_REGISTER(udp);
+#if defined(_WIN32)
+            SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'mediafoundation'");
+            GST_PLUGIN_STATIC_REGISTER(mediafoundation);
+#else
+            SPDLOG_LOGGER_INFO(logger, "Registering static GStreamer plugin 'video4linux2'");
+            GST_PLUGIN_STATIC_REGISTER(video4linux2);
+#endif
 #else
             SPDLOG_LOGGER_INFO(logger, "GStreamer static plugin archives were not found; using dynamic plugin discovery");
 #endif
